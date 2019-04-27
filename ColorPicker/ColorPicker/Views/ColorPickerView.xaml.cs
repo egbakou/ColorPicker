6ca546936c59cffc394d7ac9433d9da426297a38
egbakou <egbakoukodjolaurent@gmail.com>
2019-04-27 09:18:28 +0000

    ColorPicker MainView added

---
+++ ColorPicker/ColorPicker/Views/ColorPickerView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ColorPicker.PopupPages;
+using ColorPicker.ViewModels;
+using Rg.Plugins.Popup.Extensions;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ColorPicker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ColorPickerView : ContentPage
+    {
+        private ColorPickerViewModel viewModel;
+
+        public ColorPickerView()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new ColorPickerViewModel();
+        }
+
+        private async void ShowColorsPopup(object sender, EventArgs e)
+        {
+            await Navigation.PushPopupAsync(new ColorSelectionPopupView());
+        }
+    }
+}
