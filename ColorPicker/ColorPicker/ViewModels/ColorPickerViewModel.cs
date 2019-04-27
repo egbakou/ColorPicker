ab717097a1590bd2530ec305dcd42cbd75f23036
egbakou <egbakoukodjolaurent@gmail.com>
2019-04-27 09:19:34 +0000

    BaseViewModel and ColorPickerViewModel added

---
+++ ColorPicker/ColorPicker/ViewModels/ColorPickerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ColorPicker.PopupPages;
+using Xamarin.Forms;
+
+namespace ColorPicker.ViewModels
+{
+    public class ColorPickerViewModel : BaseViewModel
+    {
+        public ColorPickerViewModel()
+        {
+            Title = "Custom ColorPicker";
+            RegisterMessages();
+        }
+
+        // Get the selected color from PopupPage using MessagingCenter
+        private void RegisterMessages()
+        {
+            MessagingCenter.Subscribe<ColorSelectionPopupViewModel>(this, "_categoryColor", (s) =>
+            {
+                if (s != null)
+                {
+                    CategoryBackgroundColor = s.SelectedColor?.HexValue;
+                }
+            });
+        }
+
+
+        private string _categoryBackgroundColor = "#2196F3";
+        public string CategoryBackgroundColor
+        {
+            get { return _categoryBackgroundColor; }
+            set { SetProperty(ref _categoryBackgroundColor, value); }
+        }
+    }
+}
