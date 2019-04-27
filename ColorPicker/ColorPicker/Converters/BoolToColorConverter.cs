0611bee3623943e1495ef0be40dd37a12ba0019b
egbakou <egbakoukodjolaurent@gmail.com>
2019-04-27 09:17:39 +0000

    Color Converters added

---
+++ ColorPicker/ColorPicker/Converters/BoolToColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ColorPicker.Converters
+{
+    public class BoolToColorConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var x = (bool)value;
+            if (x) return Color.Tomato;
+            else return Color.FromHex("#eee");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ColorPicker/ColorPicker/Converters/StringToColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ColorPicker.Converters
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var defaultColor = Color.FromRgba(0, 0, 0, 0);
+            if (value == null) return defaultColor;
+            var colorAsString = (string)value;
+            try
+            {
+                return Color.FromHex(colorAsString);
+            }
+            catch
+            {
+                return defaultColor;
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
